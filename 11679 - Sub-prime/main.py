42e90cd4e8fbe7ff564a1ed79715212629179408
Gian Vallejos <gian.vallejos92@gmail.com>
2023-09-06 17:33:05 -0500

    Sub-prime solution in py

--- 11679 - Sub-prime/main.py
+++ 11679 - Sub-prime/main.py
@@ -1,5 +1,29 @@
 import sys
 
+def read_line():
+    line = sys.stdin.readline()
+    return list(map(int, line.rstrip().split()))
+
+def evaluate_positives(banks):
+    for bank in banks:
+        if int(bank) < 0:
+           return 'N'
+    return 'S'
+
 if __name__ == '__main__':
-    
-    exit(0)
+    for line in sys.stdin:
+        B, N = line.split()[:2]
+        if B == '0' and N == '0':
+            break
+
+        banks = read_line()
+
+        for debentures in range(int(N)):
+            d, c, v = read_line()[:3]
+            banks[d-1] += v * -1
+            banks[c-1] += v
+
+        
+        print(evaluate_positives(banks))
+
+    exit (0)
